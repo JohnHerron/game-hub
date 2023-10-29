99547ce7260c3665c35e5994b8097369b4901c19
JohnHerron <herroncjohn@gmail.com>
2023-10-28 18:11:22 -0700

    Temp disable SWR to allow vercel build

--- game-hub/app/components/DiscoveryViewer.tsx
+++ game-hub/app/components/DiscoveryViewer.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React from "react";
-import useGameData from "../hooks/useGameData";
+// import useGameData from "../hooks/useGameData";
 
 export default function DiscoveryViewer() {
-  const data = useGameData();
-  console.log("response", data);
+  // const data = useGameData();
+  // console.log("response", data);
   return <div>DiscoveryViewer</div>;
 }
