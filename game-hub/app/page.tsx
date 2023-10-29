720c5672e59435deff6d3f34b4f785fa9f7db9e2
JohnHerron <herroncjohn@gmail.com>
2023-10-28 18:15:59 -0700

    Add ESLint &apos to avoid build error

--- game-hub/app/page.tsx
+++ game-hub/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h2>Discover</h2>
-      <p>Find games you've never seen!</p>
+      <p>Find games you&apos;ve never seen!</p>
       <DiscoveryViewer />
     </main>
   );
